945a0967913cb56cbcf3b3942e25fb7e0bedcb09
N-ckM <njmills@millersville.edu>
2024-10-12 14:14:57 -0400

    Oh yeah
    fixed UI by not using UImanager and just using GameManager, added multiple sprites to add to cohesivity

--- MONITIZATION IS (not) INCLUDED/Assets/Scripts/GameManager.cs
+++ MONITIZATION IS (not) INCLUDED/Assets/Scripts/GameManager.cs
@@ -7,37 +7,65 @@ using UnityEngine.UI;
 
 public class GameManager : MonoBehaviour
 {
-    public UIManager uiManager; 
-
-    public PipManager pipManager; 
-    public int playerScore = 0;
-    public int goal = 5;
-    public int money = 0;
-
     public Button newGameButton;
+    private int money = 0;
+    public Text moneyLabel;
+    public int Money{
+        get{return money;}
+        set{
+            money = value;
+            moneyLabel.GetComponent<Text>().text = "GoFundMe Balance: " + money;
+        }
+    }    
+    
+    private int score = 0;
+    public int goal = 5;
+    public Text scoreLabel;
+    public int Score{
+        get{return score;}
+        set{
+            score = value;
+            scoreLabel.GetComponent<Text>().text = "Progress: " + score + "/" + goal;
+        }
+    }
+
+    private int depth = 1; //Between 1 and 9
+    public Text depthLabel;
+    public int Depth{
+        get{return depth;}
+        set{
+            depth = value;
+            depthLabel.GetComponent<Text>().text = "Depth: " + depth;
+        }
+    }
+
 
     private bool gameOver = false;
+    public Text gameOverText;
 
     public void Start(){
-        uiManager = this.GetComponent<UIManager>();
-
-        pipManager = this.GetComponent<PipManager>();
-
         newGameButton.onClick.AddListener(LoadLevel);
+        Money = 5000;
+        Score = 0;
+        Depth = 1;
+        gameOverText.enabled = false;
+        moneyLabel.enabled = false;
+        scoreLabel.enabled = false;
+        depthLabel.enabled = false;
+
     }
 
     // Method to increase player's score
-    public void AddScore(){
-        playerScore++; 
-        uiManager.updateScore(playerScore);
-        if(playerScore>goal){
-            GameOver(true);
+    public void ChangeScore(int amount){
+        Score += amount; 
+        if(Score>goal){
+            Depth += 1;
+            goal+=10;
         }
     }
 
     public void ChangeMoney(int amount){
-        money += amount;
-        uiManager.updateMoney(money);
+        Money += amount;
         if(money<0){
             GameOver(false);
         }
@@ -48,7 +76,9 @@ public class GameManager : MonoBehaviour
     // Methods to handle game over state
     public void GameOver(bool winstatus){
         gameOver = true;
-        uiManager.displayGameOver(winstatus);
+        gameOverText.enabled = true;
+        gameOverText.text = winstatus ? "You win!" : "You lose!";
+    
         Invoke("ReturnToMenu", 2f);
     }
 
@@ -56,6 +86,9 @@ public class GameManager : MonoBehaviour
     // Public and renamed for clarity
     public void LoadLevel(){
         //uiManager.removeTitle();
+        scoreLabel.enabled = true;
+        moneyLabel.enabled = true;
+        depthLabel.enabled = true;
         SceneManager.LoadScene("GameLevel");
     }
 
@@ -64,6 +97,9 @@ public class GameManager : MonoBehaviour
     // Return to menu by loading other scene
     public void ReturnToMenu(){
         //uiManager.returnTitle();
+        scoreLabel.enabled = false;
+        moneyLabel.enabled = false;
+        depthLabel.enabled = false;
         SceneManager.LoadScene("MainMenu");
     }
 }
---
+++ MONITIZATION IS (not) INCLUDED/Assets/Scripts/Tower.cs
@@ -0,0 +1,17 @@
+using UnityEngine;
+
+public class Tower : MonoBehaviour
+{    public int cost;
+    public GameObject towerInstance;
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
