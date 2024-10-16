8cae636f5aed93e22be728f2e23755f328ad6c33
CullenIsntHere <cullen@kurtzpad.com>
2024-10-16 18:46:56 -0400

    Decrease speed scaling.

--- MONITIZATION IS (not) INCLUDED/Assets/Scripts/pipScripts/LootBoxEnemy.cs
+++ MONITIZATION IS (not) INCLUDED/Assets/Scripts/pipScripts/LootBoxEnemy.cs
@@ -10,7 +10,7 @@ public class LootBoxEnemy : Pip
         pipBody = GetComponent<Rigidbody2D>();
         GameManager gameManager = GameObject.FindObjectOfType<GameManager>();
     int depth = gameManager.getDepth();
-        speed = 0.5f + depth;
+        speed = 0.5f + (depth/4f);
         hp = 4;
         isEnemy = true;
         moneyGive = 50;
